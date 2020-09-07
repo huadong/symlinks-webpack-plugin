9088871c8cef655be162969f7edacb932f4ecfcb
Danny Zhong <lesarde@gmail.com>
2020-09-07 15:35:56 +0800

    add log output option.

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # webpack-symlink-plugin
 A webpack plugin to make symlink of other directory in source directory before compilation.
+
+## Installation
+
+```bash
+npm i -D webpack-symlink-plugin
+```
+
+## Usage
+
+In your webpack config:
+
+```js
+const SymlinkPlugin = require('webpack-symlink-plugin');
+
+module.exports = {
+  // ...etc
+  plugins: [
+    new SymlinkPlugin({ from: 'index.html', to: '200.html' })
+  ]
+};
+```
+
+This setting makes symbolic link file `[project_root]/200.html` to `[project_root]/index.html`.
+
+### You can give configurations as Array
+
+```js
+const SymlinkPlugin = require('webpack-symlink-plugin');
+
+module.exports = {
+  // ...etc
+  plugins: [
+    new SymlinkPlugin([
+      { origin: 'index.html', symlink: '200.html' },
+      { origin: 'index.html', symlink: '404.html' },
+    ])
+  ]
+};
+```
+
+### `force` option (default: `false`)
+print logs to stats instead of console.
+
+```js
+const SymlinkPlugin = require('webpack-symlink-plugin');
+
+module.exports = {
+  // ...etc
+  plugins: [
+    new SymlinkWebpackPlugin([
+      { origin: 'index.html', symlink: '200.html', force: true },
+      { origin: 'index.html', symlink: '404.html' },
+    ], { stats: true })
+  ]
+};
+```
+
+The plugin doesn't make the symlink if the destination doesn't exist as default.
+Passing the option `force: true`, will create it regardless.
+
+### `stats` option (default: `false`)
+print logs to stats instead of console.
+
+```js
+const SymlinkPlugin = require('webpack-symlink-plugin');
+
+module.exports = {
+  // ...etc
+  plugins: [
+    new SymlinkWebpackPlugin([
+      { origin: 'index.html', symlink: '200.html' },
+      { origin: 'index.html', symlink: '404.html' },
+    ], { stats: true })
+  ]
+};
+```
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import { existsSync, statSync, readlinkSync, symlinkSync, unlinkSync } from 'fs';
+import { existsSync, lstatSync, readlinkSync, symlinkSync, unlinkSync } from 'fs';
 import { mkdirsSync } from 'fs-extra';
 import { relative, resolve, dirname } from 'path';
 
 class SymlinkPlugin {
-  constructor(config = []) {
+  constructor(config = [], options = {stats: false}) {
     this.sources = Array.isArray(config) ? config : [config];
+    this.options = options || {};
   }
 
   apply(compiler) {
     const pluginName = this.constructor.name;
 
     compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
-      const logger = compilation.getLogger ? compilation.getLogger('webpack-symlink-plugin') : console;
+      const logger = (compilation.getLogger && this.options.stats) ? compilation.getLogger('webpack-symlink-plugin') : console;
 
       logger.debug('start to create symlinks.');
 
@@ -21,28 +22,43 @@ class SymlinkPlugin {
         from = resolve(baseDir, from);
         to = resolve(baseDir, to);
 
-        const relativePath = relative(dirname(to), from);
-        // create target dirs
-        mkdirsSync(dirname(to));
-        process.chdir(dirname(to));
-        // create symlink
-        let created = false;
-        if (existsSync(to)) {
-          if (statSync(to).isSymbolicLink()) {
+        if (source.force || existsSync(from)) {
+          const relativePath = relative(dirname(to), from);
+          // create target dirs
+          mkdirsSync(dirname(to));
+          process.chdir(dirname(to));
+          // create symlink
+          let created = false;
+          
+          if (existsSync(to)) {
             // check exist symlink
-            const oldPath = readlinkSync(to);
-            if (resolve(dirname(to), oldPath) !== from) {
-              unlinkSync(to);
+            const stats = lstatSync(to);
+            if (stats.isSymbolicLink()) {
+              const oldPath = readlinkSync(to);
+              if (resolve(dirname(to), oldPath) !== from) {
+                logger.info(`delete symlink ${to} -> ${oldPath}`);
+                unlinkSync(to);
+              } else {
+                logger.info(`symlink: ${to} -> ${from} already exist`);
+                created = true;
+              }
             } else {
-              created = true;
+              throw new Error(`failed to create symlink: ${to} -> ${from}, ${to} already exist.`);
             }
           } else {
-            throw new Error(`failed create symlink: ${to} -> ${from}, ${to} already exists.`);
+            try {
+              // delete broken symlink
+              unlinkSync(to);
+            } catch (e) {
+            }
           }
-        } 
-        if (!created) {
-          logger.info(`sreate symlink: ${to} -> ${from}`);
-          symlinkSync(relativePath, to);
+
+          if (!created) {
+            logger.info(`create symlink: ${to} -> ${from}`);
+            symlinkSync(relativePath, to);
+          }
+        } else {
+          logger.warn(`failed to create symlink: ${to} -> ${from}, ${from} doesn't exist.`);
         }
       });
       // restore to base dir
